53ff680738a91e456c6a7cfadff80b5617c17966
aagavin <aagavin@gmail.com>
2019-06-06 22:19:09 -0400

    tv and movie and routes

---
+++ src/api/movie.py
@@ -0,0 +1,28 @@
+from starlette.routing import Route, Router
+from starlette.requests import Request
+from starlette.responses import UJSONResponse
+from requests import Response
+from .. import reqSession, API_KEY, BASE_URL
+
+
+async def get_movie_by_id(request: Request) -> UJSONResponse:
+    movie_id = request.path_params.get('movie_id')
+    result: Response = reqSession.get(f'{BASE_URL}/movie/{movie_id}', params={'api_key': API_KEY})
+    return UJSONResponse(result.json())
+
+
+async def get_popular_movie(request: Request) -> UJSONResponse:
+    result: Response = reqSession.get(f'{BASE_URL}/movie/popular', params={'api_key': API_KEY})
+    return UJSONResponse(result.json()['results'])
+
+
+async def get_up_coming() -> UJSONResponse:
+    result: Response = reqSession.get(f'{BASE_URL}/movie/upcoming', params={'api_key': API_KEY})
+    return UJSONResponse(result.json()['results'])
+
+
+MovieRouter = Router([
+    Route('/popular', endpoint=get_popular_movie, methods=['GET']),
+    Route('/upcoming', endpoint=get_popular_movie, methods=['GET']),
+    Route('/{movie_id}', endpoint=get_movie_by_id, methods=['GET'])
+])
--- src/routes.py
+++ src/routes.py
@@ -1,6 +1,8 @@
 from starlette.routing import Mount
 from starlette.applications import Starlette
 from src.api.search import SearchRouter
+from src.api.movie import MovieRouter
+from src.api.tv import TvRouter
 from . import config
 
 
@@ -8,4 +10,10 @@ DEBUG = config('DEBUG', cast=bool)
 app: Starlette = Starlette(debug=DEBUG)
 
 
-app.routes.append(Mount('/search', app=SearchRouter))
+app.routes.extend(
+    [
+        Mount('/search', app=SearchRouter),
+        Mount('/movie', app=MovieRouter),
+        Mount('/tv', app=TvRouter),
+    ]
+)
