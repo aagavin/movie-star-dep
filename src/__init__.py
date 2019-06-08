092a4519ebab113bb21c70c9a5bda0b8fe2771cb
aagavin <aagavin@gmail.com>
2019-06-08 19:17:40 -0400

    added caching to movie route added firebase creds

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,8 @@
 from starlette.config import Config
 from starlette.datastructures import URL, Secret
 from requests import Session
+from firebase_admin import credentials
+import firebase_admin
 import requests
 
 
@@ -8,6 +10,31 @@ config = Config()
 
 API_KEY = config('API_KEY', cast=Secret)
 BASE_URL = config('BASE_URL', cast=URL)
-
+project_id = config('project_id')
+private_key_id = config('private_key_id')
+private_key = config('private_key')
+client_email = config('client_email')
+client_id = config('client_id')
+auth_uri = config('auth_uri', cast=URL)
+token_uri = config('token_uri', cast=URL)
+auth_provider_x509_cert_url = config('auth_provider_x509_cert_url', cast=URL)
+client_x509_cert_url = config('client_x509_cert_url', cast=URL)
 
 reqSession: Session = requests.session()
+
+
+cred = credentials.Certificate({
+  "type": "service_account",
+  "project_id": project_id,
+  "private_key_id": private_key_id,
+  "private_key": private_key.replace('\\\\n', '\n'),
+  "client_email": client_email,
+  "client_id": client_id,
+  "auth_uri": auth_uri,
+  "token_uri": token_uri,
+  "auth_provider_x509_cert_url": auth_provider_x509_cert_url,
+  "client_x509_cert_url": client_x509_cert_url
+})
+
+
+firebase_app = firebase_admin.initialize_app(cred)
--- src/api/movie.py
+++ src/api/movie.py
@@ -1,22 +1,26 @@
 from starlette.routing import Route, Router
 from starlette.requests import Request
 from starlette.responses import UJSONResponse
+from functools import lru_cache
 from requests import Response
 from .. import reqSession, API_KEY, BASE_URL
 
 
+@lru_cache(maxsize=64)
 async def get_movie_by_id(request: Request) -> UJSONResponse:
     movie_id = request.path_params.get('movie_id')
     result: Response = reqSession.get(f'{BASE_URL}/movie/{movie_id}', params={'api_key': API_KEY})
     return UJSONResponse(result.json())
 
 
+@lru_cache(maxsize=64)
 async def get_popular_movie(request: Request) -> UJSONResponse:
     result: Response = reqSession.get(f'{BASE_URL}/movie/popular', params={'api_key': API_KEY})
     return UJSONResponse(result.json()['results'])
 
 
-async def get_up_coming() -> UJSONResponse:
+@lru_cache(maxsize=64)
+async def get_up_coming(request: Request) -> UJSONResponse:
     result: Response = reqSession.get(f'{BASE_URL}/movie/upcoming', params={'api_key': API_KEY})
     return UJSONResponse(result.json()['results'])
 
