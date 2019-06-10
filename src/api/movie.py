1f830a7cdc0b8ba5a876d95d39ea52a694b4b652
aagavin <aagavin@gmail.com>
2019-06-09 20:05:55 -0400

    removed lru cache

--- src/api/movie.py
+++ src/api/movie.py
@@ -6,20 +6,17 @@ from requests import Response
 from .. import reqSession, API_KEY, BASE_URL
 
 
-@lru_cache(maxsize=64)
 async def get_movie_by_id(request: Request) -> UJSONResponse:
     movie_id = request.path_params.get('movie_id')
     result: Response = reqSession.get(f'{BASE_URL}/movie/{movie_id}', params={'api_key': API_KEY})
     return UJSONResponse(result.json())
 
 
-@lru_cache(maxsize=64)
 async def get_popular_movie(request: Request) -> UJSONResponse:
     result: Response = reqSession.get(f'{BASE_URL}/movie/popular', params={'api_key': API_KEY})
     return UJSONResponse(result.json()['results'])
 
 
-@lru_cache(maxsize=64)
 async def get_up_coming(request: Request) -> UJSONResponse:
     result: Response = reqSession.get(f'{BASE_URL}/movie/upcoming', params={'api_key': API_KEY})
     return UJSONResponse(result.json()['results'])
@@ -27,6 +24,6 @@ async def get_up_coming(request: Request) -> UJSONResponse:
 
 MovieRouter = Router([
     Route('/popular', endpoint=get_popular_movie, methods=['GET']),
-    Route('/upcoming', endpoint=get_popular_movie, methods=['GET']),
+    Route('/upcoming', endpoint=get_up_coming, methods=['GET']),
     Route('/{movie_id}', endpoint=get_movie_by_id, methods=['GET'])
 ])
--- src/api/tv.py
+++ src/api/tv.py
@@ -5,24 +5,24 @@ from requests import Response
 from .. import reqSession, API_KEY, BASE_URL
 
 
-async def get_movie_by_id(request: Request) -> UJSONResponse:
+async def get_tv_by_id(request: Request) -> UJSONResponse:
     movie_id = request.path_params.get('movie_id')
     result: Response = reqSession.get(f'{BASE_URL}/tv/{movie_id}', params={'api_key': API_KEY})
     return UJSONResponse(result.json())
 
 
-async def get_popular_movie(request: Request) -> UJSONResponse:
+async def get_popular_tv(request: Request) -> UJSONResponse:
     result: Response = reqSession.get(f'{BASE_URL}/tv/popular', params={'api_key': API_KEY})
     return UJSONResponse(result.json()['results'])
 
 
-async def get_up_coming() -> UJSONResponse:
+async def get_up_coming_tv(request: Request) -> UJSONResponse:
     result: Response = reqSession.get(f'{BASE_URL}/movie/upcoming', params={'api_key': API_KEY})
     return UJSONResponse(result.json()['results'])
 
 
 TvRouter = Router([
-    Route('/popular', endpoint=get_popular_movie, methods=['GET']),
-    Route('/upcoming', endpoint=get_popular_movie, methods=['GET']),
-    Route('/{movie_id}', endpoint=get_movie_by_id, methods=['GET'])
+    Route('/popular', endpoint=get_popular_tv, methods=['GET']),
+    Route('/upcoming', endpoint=get_up_coming_tv, methods=['GET']),
+    Route('/{movie_id}', endpoint=get_tv_by_id, methods=['GET'])
 ])
