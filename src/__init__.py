51d98af384e14b41ab41abdf863bafbe95237af3
aagavin <aagavin@gmail.com>
2019-06-15 13:19:55 -0400

    fixed url cast

--- src/__init__.py
+++ src/__init__.py
@@ -9,16 +9,16 @@ import requests
 config = Config()
 
 API_KEY = config('API_KEY', cast=Secret)
-BASE_URL = config('BASE_URL', cast=URL)
+BASE_URL = config('BASE_URL')
 project_id = config('project_id')
 private_key_id = config('private_key_id')
 private_key = config('private_key')
 client_email = config('client_email')
 client_id = config('client_id')
-auth_uri = config('auth_uri', cast=URL)
-token_uri = config('token_uri', cast=URL)
-auth_provider_x509_cert_url = config('auth_provider_x509_cert_url', cast=URL)
-client_x509_cert_url = config('client_x509_cert_url', cast=URL)
+auth_uri = config('auth_uri')
+token_uri = config('token_uri')
+auth_provider_x509_cert_url = config('auth_provider_x509_cert_url')
+client_x509_cert_url = config('client_x509_cert_url')
 
 reqSession: Session = requests.session()
 
