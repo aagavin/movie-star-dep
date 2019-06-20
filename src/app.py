271d581860019a6eabe365f253f1536dc61ccfb5
aagavin <aagavin@gmail.com>
2019-06-19 23:31:09 -0400

    rouiting fix... might be a bug

--- src/app.py
+++ src/app.py
@@ -1,6 +1,4 @@
 from src.routes import app
-from starlette.requests import Request
-from starlette.responses import Response
 from starlette.middleware.cors import CORSMiddleware
 from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.responses import UJSONResponse
@@ -28,6 +26,7 @@ class CacheMiddleware(BaseHTTPMiddleware):
             return_body = b''
             async for body in response.body_iterator:
                 return_body += body
+            print('no cache.. adding')
             redis_db.set(encoded, return_body, ex=ONE_WEEK_SECS)
         return await call_next(request)
 
@@ -40,24 +39,6 @@ app.add_middleware(
     allow_methods=["*"]
 )
 
-# @app.middleware("http")
-# async def dispatch(request, call_next) -> Response:
-#     encoded = base64.b64encode(f'{request.url.path}-{request.path_params}-{request.query_params}'.encode('utf-8'))
-#     if any([request.url.path.startswith(path) for path in cached_paths]):
-#         cache = redis_db.get(encoded)
-#         if cache is not None:
-#             return UJSONResponse(ujson.loads(cache))
-#         response = await call_next(request)
-#         return_body = b''
-#         async for body in response.body_iterator:
-#             return_body += body
-#         redis_db.set(encoded, return_body, ex=ONE_WEEK_SECS)
-#     return await call_next(request)
-
-
-@app.route('/')
-async def home(request: Request) -> UJSONResponse:
-    return UJSONResponse({'success': True, 'query': dict(request.query_params)})
 
 if __name__ == '__main__':
     uvicorn.run(app, port=3000)
--- src/routes.py
+++ src/routes.py
@@ -1,5 +1,7 @@
 from starlette.routing import Mount
 from starlette.applications import Starlette
+from starlette.requests import Request
+from starlette.responses import UJSONResponse
 from src.api.search import SearchRouter
 from src.api.account import AccountRouter
 from src.api.movie import MovieRouter
@@ -19,3 +21,8 @@ app.routes.extend(
         Mount('/user', app=AccountRouter)
     ]
 )
+
+
+@app.route('/')
+async def home(request: Request) -> UJSONResponse:
+    return UJSONResponse({'success': True, 'query': dict(request.query_params)})
