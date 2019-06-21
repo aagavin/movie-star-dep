a136ba215d17cfdea111a5a6c518092c91942138
aagavin <aagavin@gmail.com>
2019-06-20 21:56:21 -0400

    cache fix on search endpoint

--- src/api/search.py
+++ src/api/search.py
@@ -3,17 +3,18 @@ from starlette.requests import Request
 from starlette.responses import UJSONResponse
 from .. import reqSession, API_KEY, BASE_URL, redis_db, ONE_WEEK_SECS
 from requests import Response
+from ujson import dumps as json_dumps
 import base64
 
 
 async def search_all(request: Request):
-    encoded = base64.b64encode(f'{request.url.path}-{request.path_params}-{request.query_params}'.encode('utf-8'))
+    encoded = base64.b64encode(f'{request.url.path}-{request.query_params}'.encode('utf-8'))
     endpoint = request.path_params.get('endpoint')
     if endpoint is None or endpoint not in ['movie', 'tv']:
         endpoint = 'multi'
     response = await do_search(request.query_params.get('q'), f'/search/{endpoint}')
-    redis_db.set(encoded, response, ex=ONE_WEEK_SECS)
-    return response
+    redis_db.set(encoded, json_dumps(response), ex=ONE_WEEK_SECS)
+    return UJSONResponse(response)
 
 
 async def do_search(search_query, url):
@@ -23,7 +24,7 @@ async def do_search(search_query, url):
     }
     result: Response = reqSession.get(f'{BASE_URL}{url}', params=query_params)
     response = [x for x in result.json()['results'] if x['media_type'] in ['movie', 'tv']]
-    return UJSONResponse(response)
+    return response
 
 
 SearchRouter = Router([
