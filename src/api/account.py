aaba94a29e9373639331e67fdc7c5096cf4a4a12
aagavin <aagavin@gmail.com>
2019-07-18 22:36:53 -0400

    init get favs

--- src/api/account.py
+++ src/api/account.py
@@ -16,7 +16,16 @@ async def create_account(request: Request) -> UJSONResponse:
     return UJSONResponse({'name': user.uid})
 
 
+async def get_favourites(request: Request) -> UJSONResponse:
+    token = request.headers.get('id_token')
+    uid: dict = auth.verify_id_token(token)
+    if uid.get('user_id') is None:
+        return UJSONResponse({'err': 'sdfsdfsf'})
+
+    return UJSONResponse({id: uid})
+
 AccountRouter = Router([
     Route('/create', endpoint=create_account, methods=['POST']),
-    Route('/{user_id}', endpoint=lambda r: UJSONResponse({}), methods=['GET'])
+    Route('/favourites', endpoint=get_favourites, methods=['GET']),
+    Route('/{user_id}', endpoint=lambda r: UJSONResponse({}), methods=['GET']),
 ])
