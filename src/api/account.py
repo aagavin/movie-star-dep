11a48b86582a081b2695122b7c35adb623c66365
aagavin <aagavin@gmail.com>
2019-07-22 21:09:18 -0400

    use cached result

--- src/api/account.py
+++ src/api/account.py
@@ -3,8 +3,23 @@ from starlette.requests import Request
 from starlette.responses import UJSONResponse
 from firebase_admin.auth import AuthError
 from firebase_admin import auth, firestore
+from base64 import b64encode
+from ujson import loads
+from asyncio import gather
+from .. import redis_db
+from .media import get_media_by_id
+
 
 db = firestore.client()
+
+
+async def get_cached_result(media_id, media_type) -> dict or None:
+    # /media/tv/76892-
+    encoded = b64encode(f'/media/{media_type}/{media_id}-'.encode('utf-8'))
+    cached_value = redis_db.get(encoded)
+    if cached_value is None:
+        return None
+    return loads(cached_value)
 
 
 async def create_account(request: Request) -> UJSONResponse:
@@ -26,16 +41,22 @@ async def create_account(request: Request) -> UJSONResponse:
 
 
 async def get_favourites(request: Request) -> UJSONResponse:
-    token = request.headers.get('id_token')
+    # verify token
     try:
+        token = request.headers.get('id_token')
         uid: dict = auth.verify_id_token(token)
     except ValueError as v:
         return UJSONResponse({'error': v.args}, 400)
     user_favs = db.collection('favs').document(uid.get('user_id')).get().to_dict()
+
+    # get favouriest
+    fav_results: list = []
     for fav in user_favs:
-        if user_favs[fav]['catogery'] == 'movie':
-            print('call move')
-    return UJSONResponse({'favs': user_favs})
+        cached_result = get_cached_result(fav, user_favs[fav]['catogery'])
+        if cached_result is None:
+            fav_results.append(get_media_by_id(request, True, fav, user_favs[fav]['catogery']))
+        fav_results.append(cached_result)
+    return UJSONResponse({'favs': await gather(*fav_results)})
 
 AccountRouter = Router([
     Route('/create', endpoint=create_account, methods=['POST']),
--- src/api/media.py
+++ src/api/media.py
@@ -2,28 +2,32 @@ from starlette.routing import Route, Router
 from starlette.requests import Request
 from starlette.responses import UJSONResponse
 from requests import Response
-from .. import reqSession, API_KEY, BASE_URL, redis_db, ONE_WEEK_SECS
+from time import sleep
 from ujson import dumps as json_dumps
-import base64
+from base64 import b64encode
+from .. import reqSession, API_KEY, BASE_URL, redis_db, ONE_WEEK_SECS
 
 
-async def request_by_id(media_type, id) -> Response:
-    return reqSession.get(
-        f'{BASE_URL}/{media_type}/{id}',
-        params={'api_key': API_KEY}
+async def generate_key(request):
+    return b64encode(f'{request.url.path}-{request.query_params}'.encode('utf-8'))
+
+
+async def get_media_by_id(request: Request, get_dict=False, media_id=None, media_type=None) -> UJSONResponse or dict:
+    media_id = request.path_params.get('media_id') or media_id
+    media_type = request.path_params['media_type'] or media_type
+    result = reqSession.get(
+        f'{BASE_URL}/{media_type}/{media_id}', params={'api_key': API_KEY}
     )
-
-
-async def get_media_by_id(request: Request) -> UJSONResponse:
-    encoded = base64.b64encode(f'{request.url.path}-{request.query_params}'.encode('utf-8'))
-    media_id = request.path_params.get('media_id')
-    result = await request_by_id(request.path_params["media_type"], media_id)
-    redis_db.set(encoded, json_dumps(result.json()), ex=ONE_WEEK_SECS)
-    return UJSONResponse(result.json())
+    if get_dict:
+        return result.json()
+    else:
+        encoded = await generate_key(request)
+        redis_db.set(encoded, json_dumps(result.json()), ex=ONE_WEEK_SECS)
+        return UJSONResponse(result.json())
 
 
 async def get_popular_media(request: Request) -> UJSONResponse:
-    encoded = base64.b64encode(f'{request.url.path}-{request.query_params}'.encode('utf-8'))
+    encoded = b64encode(f'{request.url.path}-{request.query_params}'.encode('utf-8'))
     result: Response = reqSession.get(
         f'{BASE_URL}/{request.path_params["media_type"]}/popular',
         params={'api_key': API_KEY}
@@ -33,7 +37,7 @@ async def get_popular_media(request: Request) -> UJSONResponse:
 
 
 async def get_up_coming(request: Request) -> UJSONResponse:
-    encoded = base64.b64encode(f'{request.url.path}-{request.query_params}'.encode('utf-8'))
+    encoded = b64encode(f'{request.url.path}-{request.query_params}'.encode('utf-8'))
     media_type = request.path_params.get('media_type')
     media_path = 'on_the_air' if media_type == 'tv' else 'upcoming'
     result: Response = reqSession.get(
@@ -45,10 +49,11 @@ async def get_up_coming(request: Request) -> UJSONResponse:
 
 
 async def get_episodes_by_season(request: Request):
-    encoded = base64.b64encode(f'{request.url.path}-{request.query_params}'.encode('utf-8'))
+    encoded = b64encode(f'{request.url.path}-{request.query_params}'.encode('utf-8'))
     tv_id = request.path_params.get('tv_id')
     season_number = request.path_params.get('season_number')
     episode_number = request.path_params.get('episode_number')
+    sleep(.1)
     result: Response = reqSession.get(
         f'{BASE_URL}/tv/{tv_id}/season/{season_number}/episode/{episode_number}',
         params={'api_key': API_KEY}
