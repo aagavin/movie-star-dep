9f6a40eab92bde3586d2e900b3f44441a3733908
aagavin <aagavin@gmail.com>
2019-12-24 20:32:21 -0500

    new v2 route added

--- src/routes.py
+++ src/routes.py
@@ -2,10 +2,10 @@ from starlette.routing import Mount
 from starlette.applications import Starlette
 from starlette.requests import Request
 from starlette.responses import UJSONResponse
-from src.api.search import SearchRouter
+from src.api.search import SearchRouter, SearchRouter2
 from src.api.account import AccountRouter
-from src.api.media import MediaRouter
-from . import config
+from src.api.media import MediaRouter, MediaRouter2
+from . import config, reqXSession
 from .db import RedisDatabase
 
 
@@ -13,12 +13,25 @@ DEBUG = config('DEBUG', cast=bool)
 app: Starlette = Starlette(debug=DEBUG)
 
 
-app.add_event_handler('startup', RedisDatabase.open_database_connection_pool)
-app.add_event_handler('shutdown', RedisDatabase.close_database_connection_pool)
+async def startup():
+    await RedisDatabase.open_database_connection_pool()
+
+
+async def shutdown():
+    await RedisDatabase.close_database_connection_pool()
+    print('closing httpx session')
+    await reqXSession.close()
+    print('httpx session closed')
+
+
+app.add_event_handler('startup', startup)
+app.add_event_handler('shutdown', shutdown)
 
 app.routes.extend(
     [
+        Mount('/v2/search', app=SearchRouter2),
         Mount('/search', app=SearchRouter),
+        Mount('/v2/media', app=MediaRouter2),
         Mount('/media', app=MediaRouter),
         Mount('/user', app=AccountRouter)
     ]
