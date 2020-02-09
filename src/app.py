6cbf42049f15dda3bb4e79cdcce1a99c5a4853bd
aagavin <aagavin@gmail.com>
2020-02-08 21:26:24 -0500

    dont cache locally

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from starlette.middleware.cors import CORSMiddleware
 from starlette.middleware.gzip import GZipMiddleware
 from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.middleware.trustedhost import TrustedHostMiddleware
 from starlette.responses import UJSONResponse
 from src.routes import app
 import base64
@@ -10,19 +11,24 @@ import uvicorn
 
 class CacheMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request, call_next):
-        # from src.db import RedisDatabase
-        # redis_async = RedisDatabase.redis_async
-        # encoded = base64.b64encode(f'{request.url.path}-{request.query_params}'.encode('utf-8'))
-        # if any([request.url.path.startswith(path) for path in ['/search/', '/media/']]):
-        #     cache = await redis_async.get(encoded, encoding='utf-8')
-        #     if cache is not None:
-        #         response: UJSONResponse = UJSONResponse(ujson.loads(cache))
-        #         response.headers.append('X-FROM-CACHE', 'true')
-        #         return response
+        if "localhost" == request.base_url.hostname:
+            response = await call_next(request)
+            response.headers['X-FROM-CACHE'] = 'false'
+            return response
+        from src.db import RedisDatabase
+        redis_async = RedisDatabase.redis_async
+        encoded = base64.b64encode(f'{request.url.path}-{request.query_params}'.encode('utf-8'))
+        if any([request.url.path.startswith(path) for path in ['/search/', '/media/']]):
+            cache = await redis_async.get(encoded, encoding='utf-8')
+            if cache is not None:
+                response: UJSONResponse = UJSONResponse(ujson.loads(cache))
+                response.headers.append('X-FROM-CACHE', 'true')
+                return response
         return await call_next(request)
 
 
 app.add_middleware(CacheMiddleware)
+app.add_middleware(TrustedHostMiddleware, allowed_hosts=['movie.aagavin.ca', 'localhost'])
 app.add_middleware(GZipMiddleware)
 app.add_middleware(
     CORSMiddleware,
