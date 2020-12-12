26f43c67daf15d23c471a37f78593a5e9552f2e6
Aaron <aagavin@gmail.com>
2020-12-11 20:14:07 -0500

    fix merge

--- src/cron/main.py
+++ src/cron/main.py
@@ -1,20 +1,47 @@
 from cachetools.func import lru_cache
 
 from src import firebase_app
+from datetime import datetime
 from firebase_admin import firestore
-from src import imdb
+from lxml import html as lh
+from imdbpie import Imdb
+import httpx
 
-
-@lru_cache(maxsize=512)
-def get_media_next_episodes(media_id: str) -> dict:
-    next_ep_id = imdb.get_title_episodes(media_id)['base']['nextEpisode'].split('/')[2]
-    date = imdb.get_title_auxiliary(next_ep_id)
-    return date['releaseDetails']['date']
-
+BROWSER_USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0"
+reqXSession = httpx.AsyncClient()
+imdb = Imdb()
 
 db = firestore.client()
 
 userIDdoc = db.collection(u'favs').stream()
+
+
+@lru_cache(maxsize=512)
+def get_media_next_episodes(media_id: str) -> dict or None:
+    episodes = httpx.get(f'https://www.imdb.com/title/{media_id}/episodes', headers={'User-Agent': BROWSER_USER_AGENT})
+    doc = lh.document_fromstring(episodes.text)
+    ep_list = doc.find_class('eplist')[0].find_class('list_item')
+    for ep in ep_list:
+        title = ep.cssselect('a[itemprop="name"]')[0].text
+        description = ep.find_class('item_description')[0].text.strip()
+        unparsed_date = ep.find_class('airdate')[0].text.strip()
+        try:
+            air_date = datetime.strptime(unparsed_date, '%d %b %Y')
+        except ValueError:
+            if unparsed_date == '':
+                continue
+            air_date = datetime.strptime(unparsed_date, '%d %b. %Y')
+
+        date_diff = (air_date - datetime.today()).days
+
+        if 0 < date_diff < 3:
+            return {
+                "title": title,
+                "description": description,
+                "air_date": air_date
+            }
+    return None
+
 
 for userID in userIDdoc:
     print(f'Favourites for user {userID.id}')
@@ -22,7 +49,6 @@ for userID in userIDdoc:
     for user_fav in userID.to_dict().values():
         if user_fav['titleType'] in ('TV series', 'tvSeries'):
             episode_data = get_media_next_episodes(user_fav['id'])
-            print(episode_data)
-        else:
-            print('#--> ')
-            print(user_fav)
+            if episode_data is not None:
+                print(user_fav.get('title'))
+                print(episode_data)
